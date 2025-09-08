e6981ca43855914d5158a8901d63d8c3c8a28074
Sai_Prannav <saipranav3939@gmail.com>
2025-09-08 12:24:37 +0530

    Update Temprature.cpp

--- Assignment_3/Temprature.cpp
+++ Assignment_3/Temprature.cpp
@@ -7,7 +7,7 @@ void dailyTemperatures(vector<int>&T)
     stack<int>s;
     for (int i = 0; i < n; i++) {
         while (!s.empty()&& T[s.top()] < T[i]) {
-            daysOfWait[s.top()] = i - s.top();
+            outvector[s.top()] = i - s.top();
             s.pop();
         }
         s.push(i);
